5bf399055c6494ca093dae3945d805053be329ac
Fahmed2024 <100038212+Fahmedo@users.noreply.github.com>
2025-03-28 12:09:03 +0100

    commit bug fix on signin

--- frontend/app/hooks/useSignIn.js
+++ frontend/app/hooks/useSignIn.js
@@ -2,18 +2,10 @@ import API, { setTokens } from '@/utils/axios';
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 import Cookies from 'js-cookie';
-import { AppContext } from '@/context/AppContext';
-import { useContext } from 'react';
 
 const URL = 'auth';
 
 export function useSignin() {
-  // const context = useContext(AppContext);
-
-  // if (!context) {
-  //   console.log('useSignin must be used within an AppProvider');
-  // }
-  // const { setUserData } = context || {};
   return useMutation({
     mutationFn: (data) => API.post(`${URL}/sign-in`, data),
     onSuccess: (res) => {
@@ -21,6 +13,8 @@ export function useSignin() {
         ...res.data[0],
         ...res.data[1],
       };
+
+      toast.success('Sign In Successfully');
 
       // Still keep localStorage for client-side usage if needed
       localStorage.setItem('currentUser', JSON.stringify(combinedUserData));
@@ -40,8 +34,6 @@ export function useSignin() {
         access: combinedUserData.access_token,
         refresh: combinedUserData.refresh_token,
       });
-
-      toast.success('Sign In Successfully');
     },
     onError: (error) => {
       toast.error(error?.response?.data?.message);
