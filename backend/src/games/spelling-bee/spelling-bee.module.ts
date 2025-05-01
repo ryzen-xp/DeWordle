fa2b6a42526ad3d54253bd8b67fe523ac29ccec2
Ibinola <ibinolamichael1@gmail.com>
2025-05-01 18:35:48 +0100

    fix module error

--- backend/src/games/spelling-bee/spelling-bee.module.ts
+++ backend/src/games/spelling-bee/spelling-bee.module.ts
@@ -2,24 +2,15 @@ import { Module } from '@nestjs/common';
 import { WordValidatorService } from './providers/word-validator-service.service';
 import { DictionaryModule } from 'src/dictionary/dictionary.module';
 import { SpellingBeeController } from './spelling-bee.controller';
-
 import { SpellingBeeService } from './spelling-bee.service';
 import { UserModule } from './user/user.module';
 import { PuzzleModule } from './games/spelling-bee/puzzle/puzzle.module';
 import { GameResultModule } from './games/spelling-bee/game-result/game-result.module';
 
 @Module({
-  imports: [UserModule, PuzzleModule, GameResultModule],
+  imports: [UserModule, PuzzleModule, GameResultModule, DictionaryModule],
   controllers: [SpellingBeeController],
-  providers: [SpellingBeeService],
-  exports: [SpellingBeeService],
-
-
-@Module({
-  imports: [DictionaryModule],
-  providers: [WordValidatorService],
-  exports: [WordValidatorService],
-  controllers: [SpellingBeeController]
-
+  providers: [SpellingBeeService, WordValidatorService],
+  exports: [SpellingBeeService, WordValidatorService],
 })
 export class SpellingBeeModule {}
